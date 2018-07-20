880a04376a6db8b44c065cb312a6f2f738bf79a1
raumunoz <c.sa.r@hotmail.com>
2018-07-19 19:07:29 -0500

    se agregeron unos servicios que  se manda llamar cada segundos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { PosicionesService } from 'services/posiciones.service';
-
-
+import { Posicion } from '../models/posicion';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,23 +9,32 @@ import { PosicionesService } from 'services/posiciones.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor (private posiciones:PosicionesService){}
+  constructor(private posiciones: PosicionesService) { }
   title: string = 'Mapita';
   latI: number = 51.678418;
   lngI: number = 7.809007;
   lat: number = 51.678418;
   lng: number = 7.809007;
-  locacionEscogida=false;
-  escogerLocacion(event){
-    this.lat=event.coords.lat;
-    this.lng=event.coords.lng;
-    this.locacionEscogida=true;
+  locacionEscogida = false;
+  n: number = 0;
+  latIss: number = 0;
+  lngIss: number = 0;
+  private timer;
+  escogerLocacion(event) {
+    this.lat = event.coords.lat;
+    this.lng = event.coords.lng;
+    this.locacionEscogida = true;
   }
-  ngOnInit(){
-    console.log("servicio",this.posiciones.regresaDatos());
-    this.posiciones.regresaDatos().subscribe((data)=>{
-      console.log("acabo",data);
-    })
+  ngOnInit() {
+    this.timer = Observable.timer(10000);
+    this.timer.subscribe((t) => this.onTimeOut());
   }
-
+  onTimeOut() {
+    this.posiciones.moveISS().subscribe((data)=>{
+      console.log("los datos son",data);
+      this.latIss=Number(data.iss_position.latitude);
+      this.lngIss=Number(data.iss_position.longitude);
+      this.timer.subscribe((t) => this.onTimeOut());
+    });
+  }
 }
