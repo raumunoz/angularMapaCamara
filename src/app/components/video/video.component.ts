d7fd98c28d88ecaf4539238fe8cb4a98218098f7
raumunoz <c.sa.r@hotmail.com>
2018-07-23 22:18:57 -0500

    se actualizao el commit

--- src/app/components/video/video.component.ts
+++ src/app/components/video/video.component.ts
@@ -6,6 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./video.component.css']
 })
 export class VideoComponent implements OnInit {
+  stream = {
+    source: 'https://storage.googleapis.com/shaka-demo-assets/angel-one-widevine/dash.mpd',
+    licenseServers: {
+        'com.widevine.alpha': {
+            serverURL: 'https://widevine-proxy.appspot.com/proxy'
+        }
+    },
+    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...'
+}
 
   constructor() { }
 
